faaed8b1e9945f37e3ea404d70ce10452ba2e203
DawudIslam8124 <158465939+DawudIslam8124@users.noreply.github.com>
2024-03-25 15:46:14 +0000

    Update exercise_7.py

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,6 @@
 # Exercise 7
-# Your solution comes here
+integer = int(input("Enter a 4 digit number: "))
+
+first_num = integer // 1000
+second_num = ((integer // 100) % 10)
+third_num = ((integer // 10) % 10)
