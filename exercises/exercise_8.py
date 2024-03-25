9f2ef3c655d3483c82ececc811dbba79bc6f6dd3
DawudIslam8124 <158465939+DawudIslam8124@users.noreply.github.com>
2024-03-25 16:11:39 +0000

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,7 @@
 # Exercise 8
-# Your solution comes here
+integer = int(input("Enter a number: "))
+integer2 = int(input("Enter a number: "))
+integer3 = int(input("Enter a number: "))
+
+sort1 = (integer3 > integer2 > integer3) * integer3
+sort2 = (integr3 < integer2 < integer1) * 
