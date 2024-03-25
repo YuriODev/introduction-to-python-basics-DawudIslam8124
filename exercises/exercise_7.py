1aab2f33bed136c1921515bfd6253de494ee3572
DawudIslam8124 <158465939+DawudIslam8124@users.noreply.github.com>
2024-03-25 15:49:29 +0000

    Update exercise_7.py

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -4,3 +4,6 @@ integer = int(input("Enter a 4 digit number: "))
 first_num = integer // 1000
 second_num = ((integer // 100) % 10)
 third_num = ((integer // 10) % 10)
+fourth_num = integer % 10
+
+print(first_num + second_num + third_num + fourth_num)
