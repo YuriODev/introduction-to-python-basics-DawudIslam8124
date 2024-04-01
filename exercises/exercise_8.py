361b4c56155cd6a2128d8f3d39ce71f1cb95cc21
DawudIslam8124 <158465939+DawudIslam8124@users.noreply.github.com>
2024-04-01 21:47:53 +0100

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -4,4 +4,7 @@ integer2 = int(input("Enter a number: "))
 integer3 = int(input("Enter a number: "))
 
 sort1 = (integer3 > integer2 > integer3) * integer3
-sort2 = (integr3 < integer2 < integer1) * 
+sort2 = (integer3 < integer2 < integer1) * integer1
+sort3 = (integer2 < integer1 < integer3) * integer2
+
+print(str(sort1)) + str(sort2)) + str(sort3)) )
